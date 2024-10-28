19df59207d256da48dc3c30e3b1455c6b244fc65
zorklin <well.well.well.pink@gmail.com>
2024-10-28 02:28:40 +0200

    commit

--- Harris_Hawks_Optimization_classic/HHO.cpp
+++ Harris_Hawks_Optimization_classic/HHO.cpp
@@ -23,88 +23,87 @@ int rand_int(std::mt19937& generator, int low, int high) {
     return range(generator);
 }
 
-HHO* initialization_hawks(int T, int size, long double max, long double min, int dimension, long double (*function)(std::vector<long double>)) {
-    HHO* hho = new HHO();
-    if (hho == nullptr) exit(EXIT_FAILURE);
-    hho->T = T;
-    hho->size = size;
-    hho->max = max;
-    hho->min = min;
-    hho->dimension = dimension;
-    hho->fitness = function;
-    hho->hawks.resize(size);
+HHO initialization_hawks(int T, int size, long double max, long double min, int dimension, long double (*function)(std::vector<long double>)) {
+    HHO hho;
+    hho.T = T;
+    hho.size = size;
+    hho.max = max;
+    hho.min = min;
+    hho.dimension = dimension;
+    hho.fitness = function;
+    hho.hawks.resize(size);
 
     std::vector<int> bases = sieve_eratosthenes(dimension);
     for (int i = 0; i < size; i++) {
         hawk temp = halton_sequence(i, bases);
-        hho->hawks[i] = temp * (max - min) + min;
-        hho->hawks[i].fitness = hho->fitness(hho->hawks[i].X);
+        hho.hawks[i] = temp * (max - min) + min;
+        hho.hawks[i].fitness = hho.fitness(hho.hawks[i].X);
     }
 
     return hho;
 }
 
-void border_correction(HHO* hho, int index) {
-    long double diff = hho->max - hho->min;
-    for (int i = 0; i < hho->dimension; i++) {
-        hho->hawks[index].X[i] = std::max(std::min(hho->hawks[index].X[i], hho->max), hho->min);
+void border_correction(HHO& hho, int index) {
+    long double diff = hho.max - hho.min;
+    for (int i = 0; i < hho.dimension; i++) {
+        hho.hawks[index].X[i] = std::max(std::min(hho.hawks[index].X[i], hho.max), hho.min);
     }
 }
 
-hawk best_hawk(HHO* hho) {
-    hawk best_hawk = hho->hawks[0];
-    for (int i = 0; i < hho->size; i++) {
-        if (hho->hawks[i].fitness < best_hawk.fitness) {
-            best_hawk = hho->hawks[i];
+hawk best_hawk(HHO& hho) {
+    hawk best_hawk = hho.hawks[0];
+    for (int i = 0; i < hho.size; i++) {
+        if (hho.hawks[i].fitness < best_hawk.fitness) {
+            best_hawk = hho.hawks[i];
         }
     }
     return best_hawk;
 }
 
-hawk calculate_Xm(HHO* hho) {
-    hawk average_hawk(0.0l, hho->dimension);
-    for (int i = 0; i < hho->size; i++) {
-        average_hawk = average_hawk + hho->hawks[i];
+hawk calculate_Xm(HHO& hho) {
+    hawk average_hawk(0.0l, hho.dimension);
+    for (int i = 0; i < hho.size; i++) {
+        average_hawk = average_hawk + hho.hawks[i];
     }
-    return average_hawk / hho->size;
+    return average_hawk / hho.size;
 }
 
-long double calculate_energy(HHO* hho, int t, std::mt19937& generator) {
-    long double E = pow(cos(PI * pow(static_cast<long double>(t) / hho->T, 2.0l / 3.0l)), 5.0l) + 1.0l;
+long double calculate_energy(HHO& hho, int t, std::mt19937& generator) {
+    long double E = pow(cos(PI * pow(static_cast<long double>(t) / hho.T, 2.0l / 3.0l)), 5.0l) + 1.0l;
     return E * rand_real(generator, -1.0, 1.0);
 }
 
-void very_high_energy(HHO* hho, int index, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator) {
+void very_high_energy(HHO& hho, int index, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator) {
     long double q = rand_real(generator, 0.0l, 1.0l);
     if (q >= 0.5) {
-        int rand_index = rand_int(generator, 0, hho->size - 1);
-        hawk rand_hawk = hho->hawks[rand_index];
-        hawk hawk_deviation = (rand_hawk - hho->hawks[index] * 2.0l * rand_real(generator, 0.0l, 1.0l)).module_number();
-        hho->hawks[index] = rand_hawk - hawk_deviation * rand_real(generator, 0.0l, 1.0l);
+        int rand_index = rand_int(generator, 0, hho.size - 1);
+        hawk rand_hawk = hho.hawks[rand_index];
+        hawk hawk_deviation = (rand_hawk - hho.hawks[index] * 2.0l * rand_real(generator, 0.0l, 1.0l)).module_number();
+        hho.hawks[index] = rand_hawk - hawk_deviation * rand_real(generator, 0.0l, 1.0l);
     }
     else {
-        long double correction = hho->min + (hho->max - hho->min) * rand_real(generator, 0.0l, 1.0l);
-        hho->hawks[index] = leader_hawk - averege_hawk - correction * rand_real(generator, 0.0l, 1.0l);
+        long double correction = hho.min + (hho.max - hho.min) * rand_real(generator, 0.0l, 1.0l);
+        hho.hawks[index] = leader_hawk - averege_hawk - correction * rand_real(generator, 0.0l, 1.0l);
     }
 }
 
-void high_energy_high_chance(HHO* hho, int index, long double E, hawk leader_hawk, std::mt19937& generator) {
+void high_energy_high_chance(HHO& hho, int index, long double E, hawk leader_hawk, std::mt19937& generator) {
     long double J = rand_real(generator, 0.0l, 1.0l);
-    hawk diff_hawk = leader_hawk - hho->hawks[index];
-    hho->hawks[index] = diff_hawk - (leader_hawk * J - hho->hawks[index]).module_number() * E;
+    hawk diff_hawk = leader_hawk - hho.hawks[index];
+    hho.hawks[index] = diff_hawk - (leader_hawk * J - hho.hawks[index]).module_number() * E;
 }
 
-void low_energy_high_chance(HHO* hho, int index, long double E, hawk leader_hawk) {
-    hawk diff_hawk = leader_hawk - hho->hawks[index];
-    hho->hawks[index] = leader_hawk - diff_hawk.module_number() * E;
+void low_energy_high_chance(HHO& hho, int index, long double E, hawk leader_hawk) {
+    hawk diff_hawk = leader_hawk - hho.hawks[index];
+    hho.hawks[index] = leader_hawk - diff_hawk.module_number() * E;
 }
 
-hawk better(HHO* hho, hawk f_hawk, hawk s_hawk) {
-    if (hho->fitness(f_hawk.X) < hho->fitness(s_hawk.X)) return f_hawk;
+hawk better(HHO& hho, hawk f_hawk, hawk s_hawk) {
+    if (hho.fitness(f_hawk.X) < hho.fitness(s_hawk.X)) return f_hawk;
     return s_hawk;
 }
 
-long double levy_flight(HHO* hho, std::mt19937& generator) {
+long double levy_flight(std::mt19937& generator) {
     long double u = rand_real(generator, 0.0l, 1.0l), v = rand_real(generator, 0.0l, 1.0l);
     long double numerator = tgamma(1.0l + beta) * sin((PI * beta) / 2.0l);
     long double denominator = tgamma((1.0l + beta) / 2.0l) * beta * pow(2.0l, (beta - 1.0l) / 2.0l);
@@ -112,45 +111,45 @@ long double levy_flight(HHO* hho, std::mt19937& generator) {
     return 0.01l * ((u * sigma) / pow(fabs(v), 1.0l / beta));
 }
 
-void high_energy_low_chance(HHO* hho, int t, int index, long double E, hawk leader_hawk, std::mt19937& generator) {
+void high_energy_low_chance(HHO& hho, int t, int index, long double E, hawk leader_hawk, std::mt19937& generator) {
     long double J = rand_real(generator, 0.0l, 1.0l);
-    hawk y_hawk = leader_hawk - (leader_hawk * J - hho->hawks[index]).module_number() * E;
-    hawk rand_hawk(0.0l, hho->dimension);
-    for (int i = 0; i < hho->dimension; i++) {
-        rand_hawk.X[i] = rand_real(generator, hho->min, hho->max);
+    hawk y_hawk = leader_hawk - (leader_hawk * J - hho.hawks[index]).module_number() * E;
+    hawk rand_hawk(0.0l, hho.dimension);
+    for (int i = 0; i < hho.dimension; i++) {
+        rand_hawk.X[i] = rand_real(generator, hho.min, hho.max);
     }
-    hawk z_hawk = y_hawk + rand_hawk * levy_flight(hho, generator);
-    hho->hawks[index] = better(hho, better(hho, y_hawk, hho->hawks[index]), z_hawk);
+    hawk z_hawk = y_hawk + rand_hawk * levy_flight(generator);
+    hho.hawks[index] = better(hho, better(hho, y_hawk, hho.hawks[index]), z_hawk);
 }
 
-void low_energy_low_chance(HHO* hho, int t, int index, long double E, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator) {
+void low_energy_low_chance(HHO& hho, int t, int index, long double E, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator) {
     long double J = rand_real(generator, 0.0l, 1.0l);
-    hawk rand_hawk(0.0l, hho->dimension);
-    for (int i = 0; i < hho->dimension; i++) {
-        rand_hawk.X[i] = rand_real(generator, hho->min, hho->max);
+    hawk rand_hawk(0.0l, hho.dimension);
+    for (int i = 0; i < hho.dimension; i++) {
+        rand_hawk.X[i] = rand_real(generator, hho.min, hho.max);
     }
     hawk y_hawk = leader_hawk - (leader_hawk * J - averege_hawk).module_number() * E;
-    hawk z_hawk = y_hawk + rand_hawk * levy_flight(hho, generator);
-    hho->hawks[index] = better(hho, better(hho, y_hawk, hho->hawks[index]), z_hawk);
+    hawk z_hawk = y_hawk + rand_hawk * levy_flight(generator);
+    hho.hawks[index] = better(hho, better(hho, y_hawk, hho.hawks[index]), z_hawk);
 }
 
-void elite_opposition_based_learning(HHO* hho, std::mt19937& generator) {
-    hawk min_hawk(INFINITY, hho->dimension);
-    hawk max_hawk(-INFINITY, hho->dimension);
+void elite_opposition_based_learning(HHO& hho, std::mt19937& generator) {
+    hawk min_hawk(INFINITY, hho.dimension);
+    hawk max_hawk(-INFINITY, hho.dimension);
 
-    for (int i = 0; i < hho->dimension; i++) {
-        for (int j = 0; j < hho->size; j++) {
-            min_hawk.X[i] = std::min(min_hawk.X[i], hho->hawks[j].X[i]);
-            max_hawk.X[i] = std::max(max_hawk.X[i], hho->hawks[j].X[i]);
+    for (int i = 0; i < hho.dimension; i++) {
+        for (int j = 0; j < hho.size; j++) {
+            min_hawk.X[i] = std::min(min_hawk.X[i], hho.hawks[j].X[i]);
+            max_hawk.X[i] = std::max(max_hawk.X[i], hho.hawks[j].X[i]);
         }
     }
 
-    for (int i = 0; i < hho->size; i++) {
-        hawk temp_hawk = hho->hawks[i];
-        for (int j = 0; j < hho->dimension; j++) {
+    for (int i = 0; i < hho.size; i++) {
+        hawk temp_hawk = hho.hawks[i];
+        for (int j = 0; j < hho.dimension; j++) {
             temp_hawk.X[j] = rand_real(generator, min_hawk.X[j], max_hawk.X[j]);
-            temp_hawk.fitness = hho->fitness(temp_hawk.X);
-            if (temp_hawk.fitness < hho->hawks[i].fitness) hho->hawks[i] = temp_hawk;
+            temp_hawk.fitness = hho.fitness(temp_hawk.X);
+            if (temp_hawk.fitness < hho.hawks[i].fitness) hho.hawks[i] = temp_hawk;
         }
     }
 }
@@ -160,29 +159,22 @@ long double gaissian_rand(long double expectations, long double deviation, std::
     return dist(generator);
 }
 
-void gaussian_walk_learning(HHO* hho, int t, hawk leader_hawk, std::mt19937& generator) {
-    hho->hawks[0] = leader_hawk;
-    for (int i = 1; i < hho->size; i++) {
-        int index = rand_int(generator, 0, hho->size - 1);
-        hawk rand_hawk = hho->hawks[index];
-        hawk tau_hawk = ((hho->hawks[i] - rand_hawk) * cos(PI / 2.0l * pow(static_cast<long double>(t) / hho->T, 2.0l))).module_number();
-        for (int j = 0; j < hho->dimension; j++) {
+void gaussian_walk_learning(HHO& hho, int t, hawk leader_hawk, std::mt19937& generator) {
+    hho.hawks[0] = leader_hawk;
+    for (int i = 1; i < hho.size; i++) {
+        int index = rand_int(generator, 0, hho.size - 1);
+        hawk rand_hawk = hho.hawks[index];
+        hawk tau_hawk = ((hho.hawks[i] - rand_hawk) * cos(PI / 2.0l * pow(static_cast<long double>(t) / hho.T, 2.0l))).module_number();
+        for (int j = 0; j < hho.dimension; j++) {
             long double deviation = std::max(tau_hawk.X[j], std::numeric_limits<long double>::min());
-            hho->hawks[i].X[j] = gaissian_rand(hho->hawks[i].X[j], deviation, generator);
+            hho.hawks[i].X[j] = gaissian_rand(hho.hawks[i].X[j], deviation, generator);
         }
-    }
-}
-
-void delete_data(HHO* hho) {
-    if (hho != nullptr) {
-        hho->hawks.clear();
-        delete hho;
     }
 }
 
 std::vector<long double> harris_hawks_optimazation(int T, int size, long double max, long double min, int dimension, long double (*function)(std::vector<long double>)) {
     std::mt19937 generator = initialization_rand();
-    HHO* hho = initialization_hawks(T, size, max, min, dimension, function);
+    HHO hho = initialization_hawks(T, size, max, min, dimension, function);
     hawk best_solution(INFINITY, dimension);
     best_solution.fitness = INFINITY;
     long double stagnation = 0;
@@ -196,7 +188,7 @@ std::vector<long double> harris_hawks_optimazation(int T, int size, long double 
                 long double E = calculate_energy(hho, t, generator);
                 very_high_energy(hho, i, leader_hawk, average_hawk, generator);
                 border_correction(hho, i);
-                hho->hawks[i].fitness = hho->fitness(hho->hawks[i].X);
+                hho.hawks[i].fitness = hho.fitness(hho.hawks[i].X);
             }
             stagnation = 0;
         }
@@ -212,7 +204,7 @@ std::vector<long double> harris_hawks_optimazation(int T, int size, long double 
                     else if (fabs(E) < 0.5l && q < 0.5l) low_energy_low_chance(hho, t, i, E, leader_hawk, average_hawk, generator);
                 }
                 border_correction(hho, i);
-                hho->hawks[i].fitness = hho->fitness(hho->hawks[i].X);
+                hho.hawks[i].fitness = hho.fitness(hho.hawks[i].X);
             }
         }
         hawk iter_solution = leader_hawk;
@@ -222,6 +214,5 @@ std::vector<long double> harris_hawks_optimazation(int T, int size, long double 
             best_solution = iter_solution;
         }
     }
-    delete_data(hho);
     return best_solution.X;
 }
--- Harris_Hawks_Optimization_classic/HHO.h
+++ Harris_Hawks_Optimization_classic/HHO.h
@@ -9,14 +9,14 @@
 struct hawk {
     std::vector<long double> X;
     long double fitness;
-    
+
     hawk() : fitness(0.0) {}
     hawk(long double value, size_t size) : X(size, value), fitness(0) {}
     hawk(const std::vector<long double>& values) : X(values), fitness(0) {}
 
     hawk operator+(const hawk& other) const {
         hawk new_hawk = *this;
-        
+
         std::transform(X.begin(), X.end(), other.X.begin(), new_hawk.X.begin(), std::plus<>());
         return new_hawk;
     }
@@ -68,20 +68,19 @@ struct HHO {
 std::mt19937 initialization_rand();
 long double rand_real(std::mt19937& generator, long double low, long double high);
 int rand_int(std::mt19937& generator, int low, int high);
-HHO* initialization_hawks(int T, int size, long double max, long double min, int dimension, long double (*function)(std::vector<long double>));
-void border_correction(HHO* hho, int index);
-hawk best_hawk(HHO* hho);
-long double calculate_energy(HHO* hho, int t, std::mt19937& generator);
-hawk calculate_Xm(HHO* hho);
-void very_high_energy(HHO* hho, int index, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator);
-void high_energy_high_chance(HHO* hho, int index, long double E, hawk leader_hawk, std::mt19937& generator);
-void low_energy_high_chance(HHO* hho, int index, long double E, hawk leader_hawk);
-hawk better(HHO* hho, hawk f_hawk, hawk s_hawk);
-long double levy_flight(HHO* hho, std::mt19937& generator);
-void high_energy_low_chance(HHO* hho, int t, int index, long double E, hawk leader_hawk, std::mt19937& generator);
-void low_energy_low_chance(HHO* hho, int t, int index, long double E, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator);
-void elite_opposition_based_learning(HHO* hho, std::mt19937& generator);
+HHO initialization_hawks(int T, int size, long double max, long double min, int dimension, long double (*function)(std::vector<long double>));
+void border_correction(HHO& hho, int index);
+hawk best_hawk(HHO& hho);
+long double calculate_energy(HHO& hho, int t, std::mt19937& generator);
+hawk calculate_Xm(HHO& hho);
+void very_high_energy(HHO& hho, int index, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator);
+void high_energy_high_chance(HHO& hho, int index, long double E, hawk leader_hawk, std::mt19937& generator);
+void low_energy_high_chance(HHO& hho, int index, long double E, hawk leader_hawk);
+hawk better(HHO& hho, hawk f_hawk, hawk s_hawk);
+long double levy_flight(std::mt19937& generator);
+void high_energy_low_chance(HHO& hho, int t, int index, long double E, hawk leader_hawk, std::mt19937& generator);
+void low_energy_low_chance(HHO& hho, int t, int index, long double E, hawk leader_hawk, hawk averege_hawk, std::mt19937& generator);
+void elite_opposition_based_learning(HHO& hho, std::mt19937& generator);
 long double gaissian_rand(long double expectations, long double deviation, std::mt19937& generator);
-void gaussian_walk_learning(HHO* hho, int t, hawk leader_hawk, std::mt19937& generator);
-void delete_data(HHO* hho);
+void gaussian_walk_learning(HHO& hho, int t, hawk leader_hawk, std::mt19937& generator);
 std::vector<long double> harris_hawks_optimazation(int T, int size, long double x_max, long double x_min, int dimension, long double (*function)(std::vector<long double>));
